4cdab6d544c6c4372399df11b3f08ac4098312d5
GunaDD <gunadigan@gmail.com>
2024-07-13 00:15:21 +0700

    fix server.js 2

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
 const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', (socket) => {
-    console.log('New client connected:', socket.id);
-    
-    socket.on('locationUpdate', (data) => {
-        io.emit('locationUpdate', { id: socket.id, latlng: data.latlng });
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
-    });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/map', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'old-index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  
+  socket.on('locationUpdate', (data) => {
+    io.emit('locationUpdate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
