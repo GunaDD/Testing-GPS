4e8f460ce189b308e94da415e18312ac7072b084
GunaDD <gunadigan@gmail.com>
2024-07-13 00:06:10 +0700

    test error

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'old-index.html'));
 });
 
 app.get('/map', (req, res) => {
